9451c7c999783a52a939850acea28ce411733088
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-06-29 21:26:38 -0400

    Added OdeToFood.Core Library and setup Restaurant model

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        public string message { get; set; }
+
         public void OnGet()
         {
-
+            message = "Hello World!";
         }
     }
 }
